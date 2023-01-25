a075c6adae3258aa6b3e6a229f70a9bd4f0a347f
Harry <babuarslan634@gmail.com>
2023-01-24 23:34:43 -0800

    All Done Series

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,9 +5,13 @@
 <div class="container custom-login">
    <div class="row">
         <div class="col-sm-4 col-sm-offset-4">
-        <form action="login" method="POST">
-                <div class="form-group">
-                    @csrf
+        <form action="register" method="POST">
+        @csrf  
+        <div class="form-group">
+                    <label for="exampleInputEmail1" class="form-label">User Name </label>
+                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="User Name">
+                </div>      
+        <div class="form-group">
                     <label for="exampleInputEmail1" class="form-label">Email Address</label>
                     <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email Address">
                 </div>
@@ -15,8 +19,8 @@
                     <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
-                <br>
-                <button type="submit" class="btn btn-primary">Login</button>
+                 <br>
+                <button type="submit" class="btn btn-primary">Register</button>
         </form>
         </div>
    </div>
